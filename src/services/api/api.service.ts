9dcf105c941193d02364d555d04540dfa6857443
Julien Pinson <pinsonj01@gmail.com>
2023-03-08 19:41:06 +0100

    Added base headers to the api service class, 'parameters' was replaced by 'headers'

--- src/services/api/api.service.ts
+++ src/services/api/api.service.ts
@@ -2,16 +2,21 @@ import { QueryParams, queryParamsToQueryString } from "../../helpers/queries/que
 import { HttpMethod } from "../../types/http/http-method.enum";
 
 export default class ApiService {
-  
-  private readonly baseUrl = 'https://jsonplaceholder.typicode.com';
-  private readonly route;
+
+  private readonly baseUrl: string = 'https://jsonplaceholder.typicode.com';
+  private readonly route: string;
+
+  private readonly baseHeaders: Headers = {
+    'Content-Type': 'application/json',
+  };
 
   constructor(route?: string) {
     this.route = route ?? '';
   }
 
   /**
-   * Performs a fetch request on the api using the given method, path and parameters.
+   * Performs a fetch request on the api using the given method and path.
+   * Additional headers can be provided, base headers can be overwritten.
    * Also accepts a body for POST / PUT / PATCH requests.
    * 
    * @param method HttpMethod
@@ -23,12 +28,19 @@ export default class ApiService {
   private async request(
     method: HttpMethod, 
     path: Path,
-    parameters: Object = {},
+    headers: Headers = {},
     body: Object = {},
   ): Promise<any> {
+    // Combine the base headers with the custom ones.
+    headers = {
+      ...this.baseHeaders,
+      ...headers,
+    };
+
     const response = await fetch(`${this.baseUrl}/${this.route}/${path}`, {
       method,
       body: JSON.stringify(body),
+      headers,
     });
 
     return response.json();
@@ -39,50 +51,55 @@ export default class ApiService {
    * 
    * @param path Path
    * @param queryParams QueryParams
+   * @param headers Headers
    * @returns Promise<any>
    */
-  protected async get(path: Path, queryParams: QueryParams = {}): Promise<any> {
+  protected async get(path: Path, queryParams: QueryParams = {}, headers: Headers = {}): Promise<any> {
     path = path + queryParamsToQueryString(queryParams);
 
-    return this.request(HttpMethod.GET, path);
+    return this.request(HttpMethod.GET, path, headers);
   }
 
   /**
    * Performs a PUT request against the api.
    * 
    * @param path Path
-   * @param parameters Object
+   * @param headers Headers
    * @param body Object
    * @returns Promise<any>
    */
-  protected async put(path: Path, parameters: Object = {}, body: Object = {}): Promise<any> {
-    return this.request(HttpMethod.PUT, path, parameters, body);
+  protected async put(path: Path, headers: Headers = {}, body: Object = {}): Promise<any> {
+    return this.request(HttpMethod.PUT, path, headers, body);
   }
 
   /**
  * Performs a PATCH request against the api.
  * 
  * @param path Path
- * @param parameters Object
+ * @param headers Headers
  * @param body Object
  * @returns Promise<any>
  */
-  protected async patch(path: Path, parameters: Object = {}, body: Object = {}): Promise<any> {
-    return this.request(HttpMethod.PATCH, path, parameters, body);
+  protected async patch(path: Path, headers: Headers = {}, body: Object = {}): Promise<any> {
+    return this.request(HttpMethod.PATCH, path, headers, body);
   }
 
 /**
  * Performs a DELETE request against the api.
  * 
  * @param path Path
- * @param parameters Object
+ * @param headers Headers
  * @returns Promise<any>
  */
-  protected async delete(path: Path, parameters: Object = {}): Promise<any> {
-    return this.request(HttpMethod.PUT, path, parameters);
+  protected async delete(path: Path, headers: Headers = {}): Promise<any> {
+    return this.request(HttpMethod.PUT, path, headers);
   }
 
 }
 
 // Types
 type Path = string|number;
+
+interface Headers {
+  [key: string]: string;
+}

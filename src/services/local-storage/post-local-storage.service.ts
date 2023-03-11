5b1ae5b87f6b16068be15b9229f05fe88ce6e393
Julien Pinson <dinkertons@gmail.com>
2023-03-11 13:01:07 +0100

    PostLocalStorageService: Documented the methods of the class.

--- src/services/local-storage/post-local-storage.service.ts
+++ src/services/local-storage/post-local-storage.service.ts
@@ -7,11 +7,23 @@ export default class PostLocalStorageService extends LocalStorageService {
     super('posts');
   }
 
+  /**
+   * Returns the array of posts stored in the localStorage.
+   * 
+   * @returns Post[]
+   */
   public getAll(): Post[] {
     const posts = this.getItem<Post[]>();
     return posts ?? [];
   }
 
+  /**
+   * Returns the post with the specified id stored in the localStorage.
+   * Returns null if the post wasn't found.
+   * 
+   * @param id Post['id']
+   * @returns Post | null
+   */
   public getById(id: Post['id']): Post | null {
     const posts = this.getAll();
     const post = posts.find((post: Post) => post.id === id);
@@ -19,11 +31,25 @@ export default class PostLocalStorageService extends LocalStorageService {
     return post ?? null;    
   }
 
+  /**
+   * Returns the last post stored in the localStorage.
+   * Returns null if the array is empty.
+   * 
+   * @returns Post | null
+   */
   public getLast(): Post | null {
     const posts = this.getAll();
     return posts.pop() ?? null;
   }
 
+  /**
+   * Store the given posts in the localStorage.
+   * If the store is empty => store without any change.
+   * If the store had posts => store with autoincrementing the post id.
+   * 
+   * @param ...posts Post[]
+   * @returns void
+   */
   public add(...posts: Post[]): void {
     const storedPosts = this.getAll();
 
@@ -47,6 +73,14 @@ export default class PostLocalStorageService extends LocalStorageService {
     this.setItem<Post[]>([...storedPosts, ...newPosts]);
   }
 
+  /**
+   * Update the given post stored in the localStorage.
+   * Returns true on success.
+   * Returns false if the post wasn't found in the localStorage.
+   * 
+   * @param post Post
+   * @returns boolean
+   */
   public update(post: Post): boolean {
     const posts = this.getAll();
     const index = posts.findIndex((p: Post) => p.id === post.id);
@@ -61,6 +95,14 @@ export default class PostLocalStorageService extends LocalStorageService {
     return true;
   }
 
+  /**
+   * Delete a post stored in the localStorage using the given id.
+   * Returns true on success.
+   * Returns false if the post wasn't found in the localStorage.
+   * 
+   * @param id Post['id']
+   * @returns boolean
+   */
   public delete(id: Post['id']): boolean {
     const posts = this.getAll();
     const index = posts.findIndex((post: Post) => post.id === id);
